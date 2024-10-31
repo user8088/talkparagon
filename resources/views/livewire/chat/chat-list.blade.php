496252f59f2a7e6efcc0ddddddd7f6e371337a29
Asim <asimmahmood11110@gmail.com>
2024-10-31 11:49:16 +0500

    chat list header and buttons created > Frontend

--- resources/views/livewire/chat/chat-list.blade.php
+++ resources/views/livewire/chat/chat-list.blade.php
@@ -1,3 +1,28 @@
-<div>
-    chatlist
+<div
+ x-data="{type:'all'}"
+ class="flex flex-col transition-all h-full overflow-hidden">
+    <header class="px-3 z-10 bg-fuchsia-950 sticky top-0 w-full py-2">
+        <div class="border-b justify-between flex items-center pb-2">
+            <div class="flex items-center gap-2">
+                <h5 class="font-extrabold text-2xl text-white">Chats</h5>
+            </div>
+            <button>
+                <svg class="w-7 h-7" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white"  viewBox="0 0 16 16">
+                    <path d="M14 10.5a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0 0 1h3a.5.5 0 0 0 .5-.5m0-3a.5.5 0 0 0-.5-.5h-7a.5.5 0 0 0 0 1h7a.5.5 0 0 0 .5-.5m0-3a.5.5 0 0 0-.5-.5h-11a.5.5 0 0 0 0 1h11a.5.5 0 0 0 .5-.5"/>
+                </svg>
+            </button>
+        </div>
+        {{-- Filters --}}
+        <div class="flex gap-3 items-center overflow-x-scroll p-2 bg-fuchsia-950" style="scrollbar-width: none;">
+            <button @click="type='all'" :class="{'bg-indigo-400 border-0 text-black':type=='all'}" class="inline-flex justify-center item-center rounded-full gap-x-1 text-xs font-medium px-3 lg:px-5 py-1  lg:py-2.5 border text-white border">
+                All
+            </button>
+            <button @click="type='deleted'" :class="{'bg-indigo-400 border-0 text-black':type=='deleted'}" class="inline-flex justify-center item-center rounded-full gap-x-1 text-xs font-medium px-3 lg:px-5 py-1  lg:py-2.5 border text-white border">
+                Deleted
+            </button>
+        </div>
+    </header>
+    <main>
+
+    </main>
 </div>
--- resources/views/livewire/chat/index.blade.php
+++ resources/views/livewire/chat/index.blade.php
@@ -1,5 +1,5 @@
 <div class="fixed h-full flex bg-purple  lg:shadow-sm overflow-hidden inset-0 lg:top-16  m-auto lg:h-[90%] rounded-lg">
-    <div class="relative w-full md:w-[320px] xl:w-[400px] overflow-y-auto shrink-0 h-full bg-fuchsia-950 rounded-lg">
+    <div class="relative w-full md:w-[320px] xl:w-[400px] overflow-y-auto shrink-0 h-full border bg-fuchsia-950 rounded-lg">
         @include('livewire.chat.chat-list')
     </div>
     <div class="hidden md:grid w-full  bg-gradient-to-tl from-pink-600 to-violet-600 rounded-lg  opacity-100 h-full relative overflow-y-auto rounded-lg" style="contain: content">
